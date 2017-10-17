cafd082fcad31265cf2c02531fca35a59e7fb511
Maarten Paauw <maartenpaauw@gmail.com>
2017-10-17 11:44:15 +0200

    chore(toon): update to new version and added binary_sensor folder

--- toon.sh
+++ toon.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cp ./libraries/toon/toon.py ./custom_components/toon.py
+cp ./libraries/toon/binary_sensor/toon.py ./custom_components/binary_sensor/toon.py
 cp ./libraries/toon/climate/toon.py ./custom_components/climate/toon.py
 cp ./libraries/toon/sensor/toon.py ./custom_components/sensor/toon.py
 cp ./libraries/toon/switch/toon.py ./custom_components/switch/toon.py
