d92fc0cdfbc5982165ab85da014f7b2a2dfcda06
Maarten Paauw <maartenpaauw@gmail.com>
2018-01-16 12:31:02 +0100

    fix: use te specific version from the version file

--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 pip install homeassistant --upgrade;
 git add .HA_VERSION;
-git commit -m "chore(home-assistant): upgrade home assistant to version $(hass --version)";
+git commit -m "chore(home-assistant): upgrade home assistant to version $(cat .HA_VERSION)";
 git push origin master;
