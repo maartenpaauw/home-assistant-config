c5815b348ca0ae63da7d02ddc8427fe45ce7412d
Maarten Paauw <maartenpaauw@gmail.com>
2018-02-11 21:34:39 +0100

    chore: no need for a homeassistant local installation

--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-pip install homeassistant --upgrade;
 git add .HA_VERSION;
 git commit -m "chore(home-assistant): upgrade home assistant to version $(cat .HA_VERSION)";
 git push origin master;
