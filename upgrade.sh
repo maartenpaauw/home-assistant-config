4f668cbdd1e653dac5be3f067829c340cfe57a32
Maarten Paauw <maartenpaauw@gmail.com>
2018-02-28 20:00:39 +0100

    Revert "chore: no need for a homeassistant local installation"
    This reverts commit c5815b348ca0ae63da7d02ddc8427fe45ce7412d.

--- upgrade.sh
+++ upgrade.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+pip install homeassistant --upgrade;
 git add .HA_VERSION;
 git commit -m "chore(home-assistant): upgrade home assistant to version $(cat .HA_VERSION)";
 git push origin master;
