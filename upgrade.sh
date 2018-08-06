9e17d0703db47546b16c8d42c035119bbc05f860
Maarten Paauw <maartenpaauw@gmail.com>
2018-08-06 15:16:57 +0200

    chore: create tag from home assistant version

--- upgrade.sh
+++ upgrade.sh
@@ -2,4 +2,5 @@
 pip install homeassistant --upgrade;
 git add .HA_VERSION;
 git commit -m "chore(home-assistant): upgrade home assistant to version $(cat .HA_VERSION)";
-git push origin master;
+git tag $(cat .HA_VERSION) -a "Home Assistant $(cat .HA_VERSION)";
+git push origin master --tags;
