c60b1bc50728a08b808be7bb458307e1a07f8533
maartenpaauw <maartenpaauw@gmail.com>
2018-08-07 20:56:43 +0200

    fix: tag message

--- upgrade.sh
+++ upgrade.sh
@@ -2,5 +2,5 @@
 pip install homeassistant --upgrade;
 git add .HA_VERSION;
 git commit -m "chore(home-assistant): upgrade home assistant to version $(cat .HA_VERSION)";
-git tag $(cat .HA_VERSION) -a "Home Assistant $(cat .HA_VERSION)";
+git tag $(cat .HA_VERSION) -m "Home Assistant $(cat .HA_VERSION)";
 git push origin master --tags;
