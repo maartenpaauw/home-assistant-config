8327e83b0dcec8f471640e1e8ec1c0dd85c4754e
maartenpaauw <maartenpaauw@gmail.com>
2018-09-22 17:31:46 +0200

    ci: download specific version #74

--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-pip install homeassistant --upgrade;
+pip install -I homeassistant==$(cat .HA_VERSION);
 git add .HA_VERSION;
 git commit -m "chore(home-assistant): upgrade home assistant to version $(cat .HA_VERSION)";
 git tag $(cat .HA_VERSION) -m "Home Assistant $(cat .HA_VERSION)";
