6938feb9aa5d85421697a74c869924013e2e2ed6
maartenpaauw <maartenpaauw@gmail.com>
2019-04-10 16:01:43 +0200

    fix: typo

--- update-hass.sh
+++ update-hass.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 pip install -I homeassistant==$(cat .HA_VERSION);
 git add .HA_VERSION;
-git commit -m "chore(home-assistant): upgrade home assistant to version $(cat .HA_VERSION)";
+git commit -m "chore(home-assistant): update home assistant to version $(cat .HA_VERSION)";
 git tag $(cat .HA_VERSION) -m "Home Assistant $(cat .HA_VERSION)";
 git push origin master --tags;
