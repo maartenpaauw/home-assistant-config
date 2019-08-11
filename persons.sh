abb15acb06721c107a641ef80985a402e5791cfd
maartenpaauw <maartenpaauw@gmail.com>
2019-08-12 00:30:17 +0200

    chore(persons): add persons integration

---
+++ persons.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+curl https://gravatar.com/avatar/c32c1a3574c36662aee8a65da32ca8ee --output www/persons/maarten_paauw.jpg
+curl https://gravatar.com/avatar/b18072a32099caeca3b5857337de6d2b --output www/persons/rianne_van_der_maat.jpg
