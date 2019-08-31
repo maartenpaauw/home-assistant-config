65a972bb0d79b21702355798db6af7782a3f8a7c
maartenpaauw <maartenpaauw@gmail.com>
2019-08-31 23:30:30 +0200

    chore: move all configuration files to a folder called config

--- update-hass.sh
+++ update-hass.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-pip install -I homeassistant==$(cat .HA_VERSION);
-git add .HA_VERSION;
-git commit -m "chore(home-assistant): update home assistant to version $(cat .HA_VERSION)";
-git tag $(cat .HA_VERSION) -m "Home Assistant $(cat .HA_VERSION)";
+pip install -I homeassistant==$(cat config/.HA_VERSION);
+git add config/.HA_VERSION;
+git commit -m "chore(home-assistant): update home assistant to version $(cat config/.HA_VERSION)";
+git tag $(cat config/.HA_VERSION) -m "Home Assistant $(cat config/.HA_VERSION)";
 git push origin master --tags;
